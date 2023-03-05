cc9639487ef6706dc631be3ec78431203375b365
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-06 00:25:57 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Shop from './components/Shop/Shop';
-import Extra from './components/Extra/Extra';
 
 function App() {
   return (
     <div className="App container">
       <Shop />
-      <Extra></Extra>
     </div>
   );
 }
